f029ae6173b11ee07242cd70592f58033ea2257c
RaihanKabir277 <raihank0198@gmail.com>
2025-07-12 07:56:08 +0600

    Set up the screen and test for Pong game

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 
-print("pragram starts here")
+from turtle import Turtle, Screen
+
+
+
+
+screen = Screen()
+screen.setup(800, 600)
+screen.bgcolor("black")
+screen.title("PONG GAME")
+
+
+tim = Turtle()
+tim.color("white")
+tim.shape("circle")
+tim.forward(100)
+
+
+
+
+
+
+screen.exitonclick()
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+rtle()
+tim.color("white")
+tim.shape("circle")
+tim.forward(100)
